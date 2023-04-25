ea840eeedeb23e22ab63e09b1654b8c54dca2bd1
EvilAFAS <evilafas99@gmail.com>
2023-04-25 16:49:05 -0500

    panel crew listo

--- index.js
+++
@@ -1,7 +0,0 @@
-const iconHeart = document.querySelector(".heartspan");
-const iconHeart2 = document.querySelector(".heart2");
-
-iconHeart.addEventListener('mouseover',()=>{
-    iconHeart2.classList.remove("inactive");
-    iconHeart.classList.add("inactive");    
-})
